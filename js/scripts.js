ad0f526702dc2c192a7f566979150272c4fe3704
Olivia Hinton and Shradha Pulla <shradha@>
2016-06-07 10:09:43 -0700

    Add .push to js code to make code work slightly better

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
   $("form#count-up").submit(function(event) {
+    // debugger;
 
-    var number1 = parseInt("input#number1").val();
-    var number2 = parseInt("input#number2").val();
+    var numberOne = parseInt($("input#number1").val());
+    var numberTwo = parseInt($("input#number2").val());
     // var total = [];
     // var outputList = ["#output-list"];
-    for (var output = number2; output <= number1; output *= number2) {
+
+    var output = [];
+    for (var index = numberTwo; index <= numberOne; index *= numberTwo) {
+      // outputList = output;
+      output.push(index);
     }
 
     alert(output);
